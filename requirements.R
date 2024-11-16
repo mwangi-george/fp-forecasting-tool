d9329aa5371f1ce928eaaebb69ba106a077d8d8d
George Mwangi <mwangigeorge648@gmail.com>
2024-11-16 13:58:47 +0300

    Requirements file

---
+++ requirements.R
@@ -0,0 +1,6 @@
+install.packages(c(
+    "tidyverse", "bslib", "shiny", "shinyjs", "janitor", "apexcharter",
+    "shinyWidgets", "glue", "plotly", "timetk", "shinycssloaders",
+    "shinyalert", "bsicons", "DT", "prophet", "fs", "httr", "memoise",
+    "dhis2r", "tibble"
+))
