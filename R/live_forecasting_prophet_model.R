65257d3209d8945d2fdd5a8ed4789a202f58c427
George Mwangi <mwangigeorge648@gmail.com>
2024-11-17 10:36:47 +0300

    Updated modules

--- R/live_forecasting_prophet_model.R
+++ R/live_forecasting_prophet_model.R
@@ -64,10 +64,10 @@ live_prophet_forecasting_model_page_server <- function(id, data_to_forecast) {
     })
 
     prophet_output_data <- eventReactive(input$run_forecast, {
-      withProgress(
-        expr = {
-          model_results <- suppressMessages(
-            expr = {
+      model_results <- suppressMessages(
+        withProgress(
+          expr = {
+            expr <- {
               forecast_with_prophet(
                 data_to_forecast = prophet_input_data(),
                 horizon = input$horizon,
@@ -75,8 +75,8 @@ live_prophet_forecasting_model_page_server <- function(id, data_to_forecast) {
                 show_seasonality = input$seasonality
               )
             }
-          )
-        }, min = 0, max = 10, value = 9, message = "Processing..."
+          }, min = 0, max = 10, value = 9, message = "Processing..."
+        )
       )
 
       return(model_results)
@@ -86,72 +86,84 @@ live_prophet_forecasting_model_page_server <- function(id, data_to_forecast) {
       print(glue("Type of model results: {prophet_output_data() |> class()}"))
 
       if (class(prophet_output_data()) == "character") {
-
         shinyalert("Failed", prophet_output_data(), "error", closeOnClickOutside = TRUE)
-        output$forecast_plot <- renderPlotly({NULL})
-        output$monthly_forecast <- renderDT({NULL})
-        output$yhat <- renderText({NULL})
-        output$yhat_lower <- renderText({NULL})
-        output$yhat_upper <- renderText({NULL})
-
-      } else {
         output$forecast_plot <- renderPlotly({
-          plot_ly() |>
-            add_trace(
-              data = prophet_input_data(), x = ~ds, y = ~y, type = "scatter",
-              mode = "lines+markers", name = "Actual Data",
-              line = list(color = "#E73846"), marker = list(color = "#E73846", size = 5)
-            ) |>
-            add_trace(
-              data = prophet_output_data(), x = ~ds, y = ~yhat,
-              type = "scatter", mode = "lines+markers", line = list(color = "#1C3557"),
-              marker = list(color = "#1C3557", size = 5), name = "Estimate"
-            ) |>
-            add_ribbons(
-              data = prophet_output_data(), x = ~ds, ymin = ~yhat_lower, ymax = ~yhat_upper,
-              fillcolor = "gray90", line = list(color = "transparent"), name = "Forecast Interval"
-            ) |>
-            layout(
-              title = str_c(input$org_unit_for_service_consumption_comparison, input$analytic_for_service_consumption_comparison, "Forecast Plot", sep = " "),
-              xaxis = list(title = "Date"), yaxis = list(title = "Value"), showlegend = FALSE
-            )
+          NULL
         })
-
-        forecast_table_data <- prophet_output_data() |>
-          transmute(
-            analytic_name = input$analytic_for_service_consumption_comparison,
-            org_unit = input$org_unit_for_service_consumption_comparison,
-            Date = as.Date(ds),
-            Lower = round(yhat_lower),
-            Estimate = round(yhat),
-            Upper = round(yhat_upper)
-          )
-
-        summaries <- forecast_table_data |> summarize(across(4:6, ~ sum(.x, na.rm = TRUE)))
-
-        output$yhat <- renderText({
-          summaries |>
-            pull(Estimate) |>
-            format(big.mark = ", ")
-        })
-
-        output$yhat_lower <- renderText({
-          summaries |>
-            pull(Lower) |>
-            format(big.mark = ", ")
-        })
-
-        output$yhat_upper <- renderText({
-          summaries |>
-            pull(Upper) |>
-            format(big.mark = ", ")
-        })
-
         output$monthly_forecast <- renderDT({
-          forecast_table_data |>
-            render_data_with_dt() |>
-            DT::formatCurrency(4:6, currency = "", digits = 0)
+          NULL
         })
+        output$yhat <- renderText({
+          NULL
+        })
+        output$yhat_lower <- renderText({
+          NULL
+        })
+        output$yhat_upper <- renderText({
+          NULL
+        })
+      } else {
+        withProgress(
+          expr = {
+            output$forecast_plot <- renderPlotly({
+              plot_ly() |>
+                add_trace(
+                  data = prophet_input_data(), x = ~ds, y = ~y, type = "scatter",
+                  mode = "lines+markers", name = "Actual Data",
+                  line = list(color = "#E73846"), marker = list(color = "#E73846", size = 5)
+                ) |>
+                add_trace(
+                  data = prophet_output_data(), x = ~ds, y = ~yhat,
+                  type = "scatter", mode = "lines+markers", line = list(color = "#1C3557"),
+                  marker = list(color = "#1C3557", size = 5), name = "Estimate"
+                ) |>
+                add_ribbons(
+                  data = prophet_output_data(), x = ~ds, ymin = ~yhat_lower, ymax = ~yhat_upper,
+                  fillcolor = "gray90", line = list(color = "transparent"), name = "Forecast Interval"
+                ) |>
+                layout(
+                  title = str_c(input$org_unit_for_service_consumption_comparison, input$analytic_for_service_consumption_comparison, "Forecast Plot", sep = " "),
+                  xaxis = list(title = "Date"), yaxis = list(title = "Value"), showlegend = FALSE
+                )
+            })
+
+            forecast_table_data <- prophet_output_data() |>
+              transmute(
+                analytic_name = input$analytic_for_service_consumption_comparison,
+                org_unit = input$org_unit_for_service_consumption_comparison,
+                Date = as.Date(ds),
+                Lower = round(yhat_lower),
+                Estimate = round(yhat),
+                Upper = round(yhat_upper)
+              )
+
+            summaries <- forecast_table_data |> summarize(across(4:6, ~ sum(.x, na.rm = TRUE)))
+
+            output$yhat <- renderText({
+              summaries |>
+                pull(Estimate) |>
+                format(big.mark = ", ")
+            })
+
+            output$yhat_lower <- renderText({
+              summaries |>
+                pull(Lower) |>
+                format(big.mark = ", ")
+            })
+
+            output$yhat_upper <- renderText({
+              summaries |>
+                pull(Upper) |>
+                format(big.mark = ", ")
+            })
+
+            output$monthly_forecast <- renderDT({
+              forecast_table_data |>
+                render_data_with_dt() |>
+                DT::formatCurrency(4:6, currency = "", digits = 0)
+            })
+          }, min = 0, max = 10, value = 9, message = "Building visuals..."
+        )
       }
 
       # end of observer
--- R/prepared_forecasting_results_comparison.R
+++ R/prepared_forecasting_results_comparison.R
@@ -6,7 +6,10 @@ forecast_results_consumption_page_ui <- function(id) {
       card(
         full_screen = TRUE,
         card_header("Filters"),
-        make_ui_inputs(ns)
+        make_ui_inputs(ns, date_range_end_date = forecast_results_end_date),
+        p(strong("Models Used")),
+        textOutput(ns("model_used_for_consumption")),
+        textOutput(ns("model_used_for_service"))
       ),
       card(
         full_screen = TRUE,
@@ -27,26 +30,35 @@ forecast_results_consumption_page_server <- function(id, data_to_plot) {
           analytic_name == input$analytic_for_service_consumption_comparison,
           .index |> between(input$date_range_for_service_consumption_comparison[1], input$date_range_for_service_consumption_comparison[2]),
           method %in% c(input$forecasting_approach_for_service_consumption_comparison)
-        ) |>
-        unite(col = ".method_key", method, .key, sep = " - ")
+        )
     })
 
 
     output$forecast_plot <- renderApexchart({
-        analytic <- filtered_data() |> distinct(analytic_name) |> pull(analytic_name)
-      filtered_data() |>
-        apex(
-          type = "spline",
-          mapping = aes(x = as.Date(.index), y = as.numeric(.value), group = .method_key)
-        ) |>
-        ax_labs(
-          title = glue("Forecast Plot"),
-          # subtitle = glue("Showing  data"),
-          x = "Date",
-          y = "Value"
-        ) |>
-        ax_markers(size = 6) |>
-        ax_tooltip(shared = FALSE, fillSeriesColor = TRUE, y = add_comma_sep_to_y_values())
+        suppressWarnings(
+          filtered_data() |>
+            unite(col = ".method_key", method, .key, sep = " - ") |>
+            apex(
+              type = "spline",
+              mapping = aes(x = as.Date(.index), y = as.numeric(.value), group = .method_key)
+            ) |>
+            ax_labs(
+              title = glue("Forecast Plot"),
+              # subtitle = glue("Showing  data"),
+              x = "Date",
+              y = "Value"
+            ) |>
+            ax_markers(size = 6) |>
+            ax_tooltip(shared = FALSE, fillSeriesColor = TRUE, y = add_comma_sep_to_y_values())
+        )
     })
+
+    output$model_used_for_consumption <- renderText({
+      glue("Consumption: {filtered_data() |> retrieve_model_info('Consumption')}")
+    })
+    output$model_used_for_service <- renderText({
+      glue("Service: {filtered_data() |> retrieve_model_info('Service')}")
+    })
+
   })
 }
